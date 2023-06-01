9d6525f938fdb28e15de51e8fb64dc4c40e46e68
ShahzaibKhan-cj <ShahzaibKhan-cj@users.noreply.github.com>
2023-06-01 19:55:36 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Services from './pages/Services';
-import PageNotFound from './pages/Pagenotfound';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import PageNotFound from "./pages/Pagenotfound";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-   
+    <BrowserRouter>
+      <Navbar />
 
-<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
 
-<Navbar/>
-
-<Routes>
-<Route path='/' element={<Home/>} />
-<Route path='/services' element={<Services/>} />
-<Route path='/about' element={<About/>} />
-<Route path='/contact' element={<Contact/>} />
-<Route path='*' element={<PageNotFound/>} />
-
-
-</Routes>
-
-
-<Footer/>
-
-</BrowserRouter>
-
- 
+      <Footer />
+    </BrowserRouter>
   );
 }
-
 
 export default App;
